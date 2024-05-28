43d77ee866cf94556f04411f56feb65644f5bb6c
jarwocarles <jarwocarles@gmail.com>
2024-05-28 19:49:03 +0700

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-aef7dd52-0c1f-4830-a11e-dc854df4ca87/"
 
 OUTPUT_DIR="logs"
 
